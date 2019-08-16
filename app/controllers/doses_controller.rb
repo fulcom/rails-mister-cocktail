f4fab1de5511cfa9f228d751a5484f110bc06afa
Maxime Fulconis <maximefulconis@gmail.com>
2019-08-16 11:42:46 +0200

    exo2 root

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -23,10 +23,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   def set_dose
